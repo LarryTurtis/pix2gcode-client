12ec1a32119ac0f99c34dd9dbe081ac6a70683ae
Gary Kertis <gary.kertis@gmail.com>
2021-10-22 22:18:39 -0400

    Rudimentary mouse movement

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,9 +1,22 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { isCompositeComponent } from "react-dom/test-utils";
 import { useSelector } from "react-redux";
 
 const Canvas = (props) => {
   const canvasRef = useRef(null);
   const imageUrl = useSelector((state) => state.image.imageUrl);
+
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+
+  const maybeUpdateImage = (e) => {
+    console.log(e);
+    if (isDragging) {
+      const x = pos.x + e.movementX;
+      const y = pos.y + e.movementY;
+      setPos({ x, y });
+    }
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -13,14 +26,23 @@ const Canvas = (props) => {
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
     const img = new Image();
     img.onload = function () {
-      context.drawImage(img, 0, 0); // Or at whatever offset you like
+      context.drawImage(img, pos.x, pos.y); // Or at whatever offset you like
     };
     img.src = imageUrl;
+    console.log(pos);
   });
 
   return (
     <div>
-      <canvas width={25} height={50} ref={canvasRef} {...props} />
+      <canvas
+        width={25}
+        height={50}
+        ref={canvasRef}
+        {...props}
+        onMouseDown={(e) => setIsDragging(true)}
+        onMouseUp={() => setIsDragging(false)}
+        onMouseMove={(e) => maybeUpdateImage(e)}
+      />
     </div>
   );
 };
