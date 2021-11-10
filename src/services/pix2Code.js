1ebc782caa71f21585e5b6485821254b0151caff
Gary Kertis <gary.kertis@gmail.com>
2021-11-10 14:12:32 -0500

    Fix upload url.

--- src/services/pix2Code.js
+++ src/services/pix2Code.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const pix2CodeApi = createApi({
   reducerPath: "pix2CodeApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `/upload`,
+    baseUrl: `/px2gc/upload`,
   }),
   endpoints: (builder) => ({
     main: builder.mutation({
